ce8364dc236bb066671da59e1d25a811fe304296
lola200000 <mirzoyanlola@gmail.com>
2025-07-30 15:53:15 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,11 +1,9 @@
-//5page//
+//page 5
 let count = 0;
 
 const container = document.createElement("div");
 document.body.appendChild(container);
-
-// 
-
+//nom
 const titleNom = document.createElement("h1");
 titleNom.textContent = "Nom";
 container.appendChild(titleNom);
@@ -14,28 +12,31 @@ const inputNom = document.createElement("input");
 inputNom.id = "Nom";
 inputNom.placeholder = "Nom";
 container.appendChild(inputNom);
-
+//prenom
 const titlePrenom = document.createElement("h1");
 titlePrenom.textContent = "Prenom";
-container.appendChild(titlePrenom);
 
+container.appendChild(titlePrenom);
 const inputPrenom = document.createElement("input");
 inputPrenom.id = "Prenom";
 inputPrenom.placeholder = "Prénom";
 container.appendChild(inputPrenom);
-
+//numero
 const titleNumero = document.createElement("h1");
 titleNumero.textContent = "Numero";
-
 container.appendChild(titleNumero);
 
 const inputNumero = document.createElement("input");
 inputNumero.id = "Numero";
 inputNumero.placeholder = "Numéro";
 container.appendChild(inputNumero);
+//valider
+const btnValider = document.createElement("input");
+btnValider.type = "button";
+btnValider.value = "Valider";
+btnValider.style.background = "red";
+container.appendChild(btnValider);
 
-
-//
 btnValider.addEventListener("click", () => {
   const nom = document.getElementById("Nom").value.trim();
   const prenom = document.getElementById("Prenom").value.trim();
